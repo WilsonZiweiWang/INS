26fadfee226cbb3d048851f407fa450dca8394f0
Pocket99 <48567690+Pocket99@users.noreply.github.com>
2020-01-22 20:09:04 -0500

    Update index.js

--- ins/src/index.js
+++ ins/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+consle.log('ok');
 
 
 // If you want your app to work offline and load faster, you can change
