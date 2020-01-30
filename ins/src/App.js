af66707688f580c2ee95de037fa68e0171d2614c
Ziwei Wang <ziweiwzw217@gmail.com>
2020-01-30 16:11:00 -0500

    Release branch created; basic components set up

--- ins/src/App.js
+++ ins/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './component/homePage-view';
+import NavBar from './component/NavBar';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="ins">
+        <NavBar/>
+        <Route exact path='/' component={Homepage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ins/src/component/homePage-view.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class HomePage extends Component{
+
+    render(){
+        return(
+            <div className="center" >
+                <span>Home Page</span>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
