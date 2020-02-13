6f2408407a03c4f5f4585d273864d00d52291c1d
Philippe Vo <philippe.vo.nam@gmail.com>
2020-02-12 21:28:08 -0500

    issues #21 : added image uploader component

--- ins/src/component/ImageUploader.js
+++ ins/src/component/ImageUploader.js
@@ -11,6 +11,7 @@ const ImageUploader = (props) => {
     // function to handle the uploading of image
     const imageUploadHandler = e => {
       const [file] = e.target.files; // e = event listener 
+
       if (file) {
         const reader = new FileReader(); // class helping us with uploading the picture
         const { current } = imageUploaded;
@@ -18,47 +19,25 @@ const ImageUploader = (props) => {
         reader.onload = e => {
           current.src = e.target.result;
         };
+        
         reader.readAsDataURL(file);
       }
     };
 
     // html rendered after calling the ImageUploader 
     return (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column", 
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <input
-            type="file"
-            accept="image/*"
-            onChange={imageUploadHandler} 
-            ref={imageUploader}
-            style={{
-              display: "none"
-            }}
-          />
-          <div
-            style={{
-              height: "500px",
-              width: "500px",
-              border: "1px dashed black"
-            }}
-            onClick={() => imageUploader.current.click()} // click listener for user click
-          >
-            <img 
-              ref={imageUploaded}
-              style={{
-                width: "100%",
-                height: "100%",
-                position: "acsolute"
-              }}
-            /> 
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }} >
+
+          <input type="file" accept="image/*" onChange={imageUploadHandler} ref={imageUploader} style={{ display: "none" }} />
+
+          <div style={{ height: "500px", width: "500px", border: "1px dashed black" }} onClick={() => imageUploader.current.click()} >
+            
+            <img ref={imageUploaded} style={{ width: "100%", height: "100%", position: "acsolute" }} /> 
+
           </div>
+
           Click to Upload Image
+
         </div>
       );
 }
