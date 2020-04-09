3cf79c0938a6091bb402e7bcd16f7ca5504d0571
Zhou Jiang <jz2929@gmail.com>
2020-04-08 22:33:07 -0400

    more test cases added

---
+++ test_cases/navbar/NavBar.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import SignOutButton from '../auth/SignOut'
+import * as ROUTES from '../constants/Routes';
+
+const NavBar = ({ authUser }) =>
+authUser ? (
+  <NavigationAuth authUser={authUser} />
+) : (
+  <NavigationNonAuth />
+);
+
+
+const NavigationNonAuth = () => {
+  return (
+    <div className='navbar-fixed'>
+      <nav className="nav-wrapper white">
+        <div className="container">
+          <NavLink to={ROUTES.HOME} className="brand-logo black-text">NIS</NavLink>
+          <a href="#" className="sidenav-trigger hide-on-large-only" data-target="mobile-menu">
+            <i className="material-icons black-text">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li><NavLink to={ROUTES.SIGN_IN} className="black-text">Sign In</NavLink></li>
+            <li><NavLink to={ROUTES.SIGN_UP} className="black-text">Sign Up</NavLink></li>
+          </ul>
+
+          <ul className="sidenav grey lighten-2" id="mobile-menu">
+            <li><NavLink to={ROUTES.SIGN_IN} className="black-text">Sign In</NavLink></li>
+            <li><NavLink to={ROUTES.SIGN_UP} className="black-text">Sign Up</NavLink></li>
+          </ul>
+
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+const NavigationAuth = () => {
+  return (
+    <div className='navbar-fixed'>
+      <nav className="nav-wrapper white">
+        <div className="container">
+          <NavLink to={ROUTES.HOME} className="brand-logo black-text">NIS</NavLink>
+          <a href="#" className="sidenav-trigger hide-on-large-only" data-target="mobile-menu-auth">
+            <i className="material-icons black-text">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li><NavLink to={ROUTES.HOME} className="black-text">Home</NavLink></li>
+            <li><NavLink to={ROUTES.ADD_POST} className="black-text">Add Post</NavLink></li> 
+            <li><SignOutButton /></li>
+            <li><a href={ROUTES.ACCOUNT}>
+              <i className='material-icons btn-flat transparent black-text'>
+                person_outline
+            </i>
+            </a></li>
+                       
+          </ul>
+
+          <ul className="sidenav grey lighten-2" id="mobile-menu-auth">
+            <li><NavLink to={ROUTES.HOME} className="black-text">Home</NavLink></li>
+            <li><NavLink to={ROUTES.ADD_POST} className="black-text">Add Post</NavLink></li>  
+            <li><SignOutButton /></li>
+            <li><a href={ROUTES.ACCOUNT}>
+              <i className='material-icons  transparent black-text'>
+                person_outline
+            </i>
+            </a></li>
+            
+          </ul>
+
+
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  authUser: state.sessionState.authUser,
+});
+
+export default compose(
+  connect(mapStateToProps),
+  withRouter,
+)(NavBar);
